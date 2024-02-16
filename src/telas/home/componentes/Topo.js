aef0535dbbac4ffc7347d00d4dd43b3a968323eb
Vitória Teixeira <teixeiravitoria057@gmail.com>
2024-02-15 22:12:23 -0300

    first commit

---
+++ src/telas/home/componentes/Topo.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Text, View, Image, StyleSheet } from "react-native";
+
+import logo from '../../../assets/logo.png';
+
+export default function Topo() {
+  return (
+    <View style={estilos.topo}>
+      <Image source={logo} style={estilos.imagem} />
+      <Text style={estilos.boasVindas}>Olá Vitória</Text>
+      <Text style={estilos.legenda}>Encontre os melhores produtores</Text>
+    </View>
+  );
+}
+
+const estilos = StyleSheet.create({
+  topo: {
+    backgroundColor: 'F6F6F6',
+    padding: 16,
+    marginTop: 40,
+  },
+  imagem: {
+    width: 70,
+    height: 28,
+  },
+  boasVindas: {
+    marginTop: 24,
+    fontSize: 26,
+    lineHeight: 42,
+    fontWeight: 'bold'
+  },
+  legenda: {
+    fontSize: 16,
+    lineHeight: 26,
+  }
+})
